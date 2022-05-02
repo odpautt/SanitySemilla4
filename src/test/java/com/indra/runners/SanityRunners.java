1ec40bee7977ca633759ceb5ec4b86b58ba8e337
Orlando Pautt <od.pautt@gmail.com>
2022-05-02 08:44:10 -0500

    se adiciona webdriver para linux

--- src/test/java/com/indra/runners/SanityRunners.java
+++ src/test/java/com/indra/runners/SanityRunners.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/execute.feature"
         ,glue = "com.indra.steps_definitions"
         ,snippets = SnippetType.CAMELCASE
-        ,tags="@inventory"
+        ,tags="@epos"
 )
 
 public class SanityRunners {
